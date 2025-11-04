70aaa7f0a5e471f9a8358bc1a700ada354611a08
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-04 14:30:12 +0100

    changed sivirel fields in delivery /dto and user dto

--- src/main/java/com/transports/transport/DTOS/DelivaryDto.java
+++ src/main/java/com/transports/transport/DTOS/DelivaryDto.java
@@ -1,5 +1,6 @@
 package com.transports.transport.DTOS;
 
+import com.transports.transport.entities.User;
 import com.transports.transport.enums.DeliveryStatus;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
@@ -8,41 +9,36 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DelivaryDto {
-    public interface create {}
+    public interface create {
+    }
 
-    public interface update {}
+    public interface update {
+    }
 
     @NotBlank(groups = create.class, message = "address is required")
     private String address;
 
-    @NotNull( groups = create.class , message = "latitude is required")
-    @DecimalMin(value = "-90.0", message = "Latitude must be greater than or equal to -90" ,groups = {create.class,update.class})
-    @DecimalMax(value = "90.0", message = "Latitude must be less than or equal to 90",groups = {create.class,update.class})
-    private Double latitude;
+    @NotBlank(message = "user is required", groups = {create.class, update.class})
+    private User user;
 
-    @NotNull( groups = create.class ,message = "Longitude is required" )
-    @DecimalMin(value = "-180.0", message = "Longitude must be greater than or equal to -180",groups = {create.class,update.class})
-    @DecimalMax(value = "180.0", message = "Longitude must be less than or equal to 180",groups = {create.class,update.class})
-    private Double longitude;
-
-    @NotNull(groups = create.class ,message = "Weight is required")
-    @Positive(message = "Weight must be positive",groups = {create.class,update.class})
+    @NotNull(groups = create.class, message = "Weight is required")
+    @Positive(message = "Weight must be positive", groups = {create.class, update.class})
     private Double weight;
 
-    @NotNull(groups = create.class ,message = "Volume is required")
-    @Positive(message = "Volume must be positive",groups = {create.class,update.class})
+    @NotNull(groups = create.class, message = "Volume is required")
+    @Positive(message = "Volume must be positive", groups = {create.class, update.class})
     private Double volume;
 
-    @NotBlank(groups = create.class ,message = "Preferred time slot is required")
+    @NotBlank(groups = create.class, message = "Preferred time slot is required")
     private String preferredTimeSlot;
 
-    @NotNull(groups = create.class ,message = "Status is required")
+    @NotNull(groups = create.class, message = "Status is required")
     private DeliveryStatus status;
 
-    @NotNull(groups = create.class ,message = "Sequence order is required")
-    @Min(value = 1, message = "Sequence order must be at least 1",groups = {create.class,update.class})
+    @NotNull(groups = create.class, message = "Sequence order is required")
+    @Min(value = 1, message = "Sequence order must be at least 1", groups = {create.class, update.class})
     private Integer sequenceOrder;
 
-    @NotNull(groups = create.class ,message = "Tour ID is required")
+    @NotNull(groups = create.class, message = "Tour ID is required")
     private Long tourId;
 }
--- src/main/java/com/transports/transport/DTOS/UserDto.java
+++ src/main/java/com/transports/transport/DTOS/UserDto.java
@@ -1,5 +1,7 @@
 package com.transports.transport.DTOS;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -24,9 +26,14 @@ public class UserDto {
     private String name;
     @NotBlank(message = "address is required",groups = {create.class,update.class})
     private String address;
-    @NotNull(message = "latitude is required",groups = {create.class,update.class})
+    @NotNull( groups = DelivaryDto.create.class , message = "latitude is required")
+    @DecimalMin(value = "-90.0", message = "Latitude must be greater than or equal to -90" ,groups = {DelivaryDto.create.class, DelivaryDto.update.class})
+    @DecimalMax(value = "90.0", message = "Latitude must be less than or equal to 90",groups = {DelivaryDto.create.class, DelivaryDto.update.class})
     private Double latitude;
-    @NotNull(message = "longitude is required",groups = {create.class,update.class})
+
+    @NotNull( groups = DelivaryDto.create.class ,message = "Longitude is required" )
+    @DecimalMin(value = "-180.0", message = "Longitude must be greater than or equal to -180",groups = {DelivaryDto.create.class, DelivaryDto.update.class})
+    @DecimalMax(value = "180.0", message = "Longitude must be less than or equal to 180",groups = {DelivaryDto.create.class, DelivaryDto.update.class})
     private Double longitude;
     @NotNull(message = "preferredTimeSlot is required",groups = {create.class,update.class})
     private LocalDateTime preferredTimeSlot;

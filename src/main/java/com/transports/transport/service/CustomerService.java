e14ab3b40843dfc0a245b5b800462baa21f9d91f
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-10 11:16:04 +0100

    Refactor CustomerService to use consistent variable naming and add CRUD operations

--- src/main/java/com/transports/transport/service/CustomerService.java
+++ src/main/java/com/transports/transport/service/CustomerService.java
@@ -11,29 +11,46 @@ import java.util.List;
 
 @Service
 public class CustomerService {
-    public final CustomerRepository CustomerRepository;
+    public final CustomerRepository customerRepository;
     public final CustomerMapperImpl customerMapperImpl;
 
     @Autowired
     public CustomerService(CustomerRepository CustomerRepository, CustomerMapperImpl customerMapperImpl) {
-        this.CustomerRepository = CustomerRepository;
+        this.customerRepository = CustomerRepository;
         this.customerMapperImpl = customerMapperImpl;
     }
 
     public List<CustomerDTO> findAll() {
-        List<Customer> users = CustomerRepository.findAll();
+        List<Customer> users = customerRepository.findAll();
         return users.stream().map(this.customerMapperImpl::toDTO).toList();
     }
     public CustomerDTO findById(Long id) {
-        Customer user = CustomerRepository.findById(id).orElseThrow(()->
+        Customer user = customerRepository.findById(id).orElseThrow(()->
                 new RuntimeException("user was not found ")
         );
         return this.customerMapperImpl.toDTO(user);
     }
     public CustomerDTO addUser(CustomerDTO CustomerDTO) {
         Customer user = customerMapperImpl.toEntity(CustomerDTO);
-        user = CustomerRepository.save(user);
+        user = customerRepository.save(user);
         return this.customerMapperImpl.toDTO(user);
     }
+    public String deleteById(Long id) {
+        customerRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("user was not found ")
+        );
+        customerRepository.deleteById(id);
+        return "Customer with id " + id + " was deleted";
+    }
+
+    public CustomerDTO updateById(Long id, CustomerDTO CustomerDTO) {
+        customerRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("user was not found ")
+        );
+        Customer user = customerMapperImpl.toEntity(CustomerDTO);
+        user = customerRepository.save(user);
+        return this.customerMapperImpl.toDTO(user);
+    }
+
 
 }

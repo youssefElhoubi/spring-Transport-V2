85a817194ce0e582781ee945d9150f682134651c
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-11-10 10:07:01 +0100

    added anotation to the new repos

--- src/main/java/com/transports/transport/repository/CustomerRepository.java
+++ src/main/java/com/transports/transport/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.transports.transport.repository;
 
 import com.transports.transport.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
--- src/main/java/com/transports/transport/repository/DeliveryHistoryRepository.java
+++ src/main/java/com/transports/transport/repository/DeliveryHistoryRepository.java
@@ -2,6 +2,8 @@ package com.transports.transport.repository;
 
 import com.transports.transport.entities.DeliveryHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeliveryHistoryRepository extends JpaRepository<DeliveryHistory,Long> {
 }
